0b323e5067a9ad9ffbbe635a9ede4a9c6e60a504
tristanpassaglia2-jpg <tristanpassaglia2@gmail.com>
2026-06-09 12:08:50 -0300

    Add FamilyAuth and update routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ElderChat from './pages/ElderChat';
 import FamilySetup from './pages/FamilySetup';
 import FamilyDashboard from './pages/FamilyDashboard';
+import FamilyAuth from './pages/FamilyAuth';
 import Landing from './pages/Landing';
 
 export default function App() {
@@ -10,8 +11,11 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/entrar" element={<FamilyAuth />} />
         <Route path="/setup" element={<FamilySetup />} />
+        <Route path="/configurar" element={<FamilySetup />} />
         <Route path="/chat/:elderId" element={<ElderChat />} />
+        <Route path="/chat" element={<ElderChat />} />
         <Route path="/dashboard" element={<FamilyDashboard />} />
       </Routes>
     </BrowserRouter>
