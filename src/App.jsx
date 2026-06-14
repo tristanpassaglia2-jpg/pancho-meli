c7e426d666bb646b802b054e076763da920ab700
tristanpassaglia2-jpg <tristanpassaglia2@gmail.com>
2026-06-14 14:34:30 -0300

    Add routes for Privacidad and Terminos pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FamilySetup from './pages/FamilySetup';
 import FamilyDashboard from './pages/FamilyDashboard';
 import FamilyAuth from './pages/FamilyAuth';
 import Suscribir from './pages/Suscribir';
+import Privacidad from './pages/Privacidad';
+import Terminos from './pages/Terminos';
 import Landing from './pages/Landing';
 
 export default function App() {
@@ -14,6 +16,8 @@ export default function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/entrar" element={<FamilyAuth />} />
         <Route path="/suscribir" element={<Suscribir />} />
+        <Route path="/privacidad" element={<Privacidad />} />
+        <Route path="/terminos" element={<Terminos />} />
         <Route path="/setup" element={<FamilySetup />} />
         <Route path="/configurar" element={<FamilySetup />} />
         <Route path="/chat/:elderId" element={<ElderChat />} />
